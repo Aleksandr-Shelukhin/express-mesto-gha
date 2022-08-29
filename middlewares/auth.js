1be3ca561bf4bc2633c4472b0e9b748aca454c12
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-08-29 23:31:00 +0300

    refactoring: обновить мидлвар авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
-const { JWT_SECRET } = process.env;
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthorizationError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw next(new AuthorizationError('Необходима авторизация'));
+    return next(new AuthorizationError('Необходима авторизация'));
   }
   req.user = payload;
   next();
-  return null;
 };
