2c7d13b9d21a347db6a0192cdfa1012544c40ba4
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-08-29 23:49:24 +0300

    refactoring: обновить модели карточек и пользователя

--- models/user.js
+++ models/user.js
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
+function findUserByCredentials(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
@@ -56,6 +56,7 @@ userSchema.statics.findUserByCredentials = function (email, password) {
           return user;
         });
     });
-};
+}
 
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 module.exports = mongoose.model('user', userSchema);
