22481f579764da58b8a013a507d9e57335d909e2
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-09-05 21:47:05 +0300

    features: подключить логгер запросов и ошибок

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const { signupValidate, signinValidate } = require('./middlewares/validations');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -21,6 +23,8 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 // middleware
+app.use(requestLogger); // подключаем логгер запросов
+
 app.post('/signup', signupValidate, createUser);
 app.post('/signin', signinValidate, login);
 
@@ -28,6 +32,8 @@ app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваеме данные не найдены'));
